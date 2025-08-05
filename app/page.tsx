eb11418dc75fe43a33f73feca03995f05941a33b
deanfyi <dean.curran@icloud.com>
2025-08-05 16:35:43 +0100

    add change to email

--- app/page.tsx
+++ app/page.tsx
@@ -251,7 +251,7 @@ export default function Personal() {
       >
         <h3 className="mb-5 text-lg font-medium">Connect</h3>
         <p className="mb-5 text-zinc-600 dark:text-zinc-400">
-          Feel free to drop me an email at{''}
+          Feel free to drop me an email at {''}
           <a className="underline dark:text-zinc-300" href={`mailto:${EMAIL}`}>
             {EMAIL}
           </a>
